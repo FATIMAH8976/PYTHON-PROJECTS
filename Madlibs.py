1a4fa2afc08c7d64ac778664fc293777c22a9fca
FATIMAH8976 <143977613+FATIMAH8976@users.noreply.github.com>
2025-03-06 13:42:24 +0530

    Add files via upload
    This is the sample code for Madlib in Python. Using this code, I will create a project in which you can introduce yourself using Madlibs.

---
+++ Madlibs.py
@@ -0,0 +1,9 @@
+adj= input ("Adjective :  ")
+verb1=input("Verb :" )
+verb2 = input("Verb:")
+famous_person= input("Famous person: ")
+
+madlib = f" Computer programming is so {adj} ! It Makes me so {verb1} all the time because \
+I love to {verb2} .Stay hydrated and jump like you are{famous_person} ! "
+
+print(madlib)

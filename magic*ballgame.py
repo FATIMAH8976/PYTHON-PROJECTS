3740868a2e87028da6dee8619fc90d00a5d23c15
FATIMAH8976 <143977613+FATIMAH8976@users.noreply.github.com>
2025-03-25 22:47:58 +0530

    Add files via upload

---
+++ magic*ballgame.py
@@ -0,0 +1,21 @@
+# Write code below 💖
+import random
+question=input("Question:")
+random_num=random.randint(1,9)
+
+if random_num==1:
+ print("Yes - definitely")
+elif random_num==2:
+ print("It is decidedly so")
+elif random_num==3:
+ print("Without a doubt.")
+elif random_num==4:
+ print("Reply hazy, try again.")
+elif random_num==5:
+ print("Ask again later.")
+elif random_num==6:
+ print("Better not tell you now.")
+elif random_num==7:
+ print("My sources say no.")
+else :
+ print("Very doubtful.")
